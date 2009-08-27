206009a0514097167b9ee7c074baa0f4c7c66a65
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2009-08-27 18:47:14 -0300

    Adcionados campos mágicos ao usuário

--- db/migrate/20090817183255_user_session.rb
+++ db/migrate/20090817183255_user_session.rb
@@ -3,6 +3,13 @@ class UserSession < ActiveRecord::Migration
     create_table :sessions do |t|
       t.string :session_id, :null => false
       t.text :data
+      t.integer   :login_count,         :null => false, :default => 0
+      t.integer   :failed_login_count,  :null => false, :default => 0
+      t.datetime  :last_request_at
+      t.datetime  :current_login_at
+      t.datetime  :last_login_at
+      t.string    :current_login_ip
+      t.string    :last_login_ip
       t.timestamps
     end
 
