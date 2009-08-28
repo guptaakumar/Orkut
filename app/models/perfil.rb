6aaa06e8f42f4f515fd5e027ba01e0670decfae9
Ricardo Almeida <ricardo.almeida@gonow.com.br>
2009-08-28 19:59:23 -0300

    data de nascimento do perfil

--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -1,4 +1,8 @@
 class Perfil < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user, :nome
+
+  def idade
+    ((Date.today.to_time - data_nascimento.to_time) / 1.years).to_i
+  end
 end
--- spec/models/perfil_spec.rb
+++ spec/models/perfil_spec.rb
@@ -3,4 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Perfil do
   should_belong_to :user
   should_validate_presence_of :user, :nome
+
+  it "should return perfil's age" do
+    anos_atras = 5
+    ano_nascimento = Date.today.year - anos_atras
+    data_nascimento = Date.new(ano_nascimento, Date.today.month, Date.today.day)
+    perfil = Perfil.new :data_nascimento => data_nascimento
+    perfil.idade.should == anos_atras - 1
+  end
 end
